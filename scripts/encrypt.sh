d7a6c1e0c22ac908b31128d07a589a50255c1166
Lacave Valentin <lacave.valentin@gmail.com>
2014-02-26 17:19:54 +0100

    Upload du script de base pour crypter TODO dans le fichier

---
+++ scripts/encrypt.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script will encrypt the first parameter using:
+#	*Second parameter for destination
+#	*Third parameter as passphrase (stored in a file)
+#	*Symmetric encryption
+#
+# This is how the script works:
+#	- check if he has 3 args (TODO)
+#	- check the args (TODO)
+#	- check the rights of the user (TODO)
+#	- if all is in good way, encrypt the file
+
+# TODO DEBUG mode
+# TODO test
+# TODO no asking for overwritte, check --no-batch or see batch
+# TODO no message on term
+# TODO put an extention if not exist for destination file ?
+
+#DEBUG
+echo $0 #name
+echo $1 #source
+echo $2 #destination
+echo $3 #passphrase
+#END DEBUG
+
+#-c symmetric encryption
+#-a (--armor) for ascii
+#-o output file
+#--passphrase-file file who contain passphrase
+#--no-use-agent to disable GUI and asking for passphrase
+
+gpg -ca -o $2 --passphrase-file $3  --no-use-agent $1

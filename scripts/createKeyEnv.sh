aa8f58564450849722b14107622dbdae5d9c4fec
Lacave Valentin <lacave.valentin@gmail.com>
2014-02-28 10:05:57 +0100

    Add commentary to /scripts/createKey*.s

--- scripts/createKeyEnv.sh
+++ scripts/createKeyEnv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Script who create a key with gpg and export it to env
 
 export CLEF=`gpg --gen-random --armor 1 $1`
 bash
--- scripts/createKeyTmp.sh
+++ scripts/createKeyTmp.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#Script who create a key with gpg and store it to /tmp/key
 
 gpg --gen-random -armor 1 $1 >> /tmp/key

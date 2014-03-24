292f8baf45d3f317e3f2f0c78a162c4e6492c4e6
GeoffreyUniversitySSI <guldner6u@live.fr>
2014-03-24 08:53:05 +0100

    Update EntropyClef.sh

--- entropy/EntropyClef.sh
+++ entropy/EntropyClef.sh
@@ -35,30 +35,28 @@ Dissection()
     	longueur=${#clef}
     	cmpt=0
 
-    while test $cmpt != $longueur
-    do
-	    charatest=${clef:$cmpt:1}
+	while test $cmpt != $longueur
+    	do
+	    	charatest=${clef:$cmpt:1}
 	
-	    if [ $cmpt -eq 0 ]; then
-	        tabCarac[$cmpt]=$charatest
-            tabOccu[$cmpt]=1
+	    	if [ $cmpt -eq 0 ]; then
+	        	tabCarac[$cmpt]=$charatest
+            		tabOccu[$cmpt]=1
             
-        else
-            inArray $charatest 
+        	else
+            		inArray $charatest 
 	        
-	        if [ $occu -ge 0 ]; then
-	           tabOccu[$occu]=$((${tabOccu[$occu]}+1))
+	        	if [ $occu -ge 0 ]; then
+	           	tabOccu[$occu]=$((${tabOccu[$occu]}+1))
 	           
-	        else
-                pos=${#tabCarac[*]}
-                tabCarac[$pos]=$charatest
-                tabOccu[$pos]=1
-	        fi
-	    fi
-	
-
-	    cmpt=$(($cmpt+1))
-    done
+	        	else
+                	pos=${#tabCarac[*]}
+                	tabCarac[$pos]=$charatest
+                	tabOccu[$pos]=1
+	        	fi
+	    	fi
+	    	cmpt=$(($cmpt+1))
+    	done
 }
 
 touch $2
@@ -68,43 +66,38 @@ nbKey=0
     
 while read line
 do
-    Dissection $line
+    	Dissection $line
+    	tailleTabCarac=${#tabCarac[*]}
+    	tailleTabOccu=${#tabOccu[*]}
+    	iteent=0
+    	itetailleclef=0
+    	tailleclef=0
+    	tmpmul=0
+    	tmpdiv=0
+    	entropy=0
 
-    tailleTabCarac=${#tabCarac[*]}
-    tailleTabOccu=${#tabOccu[*]}
+    	while test $itetailleclef != $tailleTabOccu
+    	do
+        	tailleclef=$(($tailleclef + ${tabOccu[$itetailleclef]}))
+        	itetailleclef=$(($itetailleclef+1))
+    	done
 
-    iteent=0
-    itetailleclef=0
-    tailleclef=0
-    tmpmul=0
-    tmpdiv=0
-    entropy=0
+    	while test $iteent != $tailleTabCarac
+    	do
+        	tmpmul=$((${tabOccu[$iteent]}*${tabOccu[$iteent]}))
+        	tmpdiv=$(bc -l <<< "$tmpmul / $tailleclef")     
+        	entropy=$(bc -l <<< "$entropy + $tmpdiv")
+        	iteent=$(($iteent+1))
+    	done 
 
-    while test $itetailleclef != $tailleTabOccu
-    do
-        tailleclef=$(($tailleclef + ${tabOccu[$itetailleclef]}))
-        itetailleclef=$(($itetailleclef+1))
-    done
-
-    while test $iteent != $tailleTabCarac
-    do
-        tmpmul=$((${tabOccu[$iteent]}*${tabOccu[$iteent]}))
-        tmpdiv=$(bc -l <<< "$tmpmul / $tailleclef")     
-        entropy=$(bc -l <<< "$entropy + $tmpdiv")
-        iteent=$(($iteent+1))
-    done 
-
-    entropy=$(bc -l <<< "1 - ($entropy / $tailleclef)")
-    entropyp=$(bc -l <<< "($entropy * $entropy)")
-
-    entropy=$(bc -l <<< "($entropy * 100)")
-    entropyp=$(bc -l <<< "($entropyp * 100)")
+    	entropy=$(bc -l <<< "1 - ($entropy / $tailleclef)")
+    	entropyp=$(bc -l <<< "($entropy * $entropy)")
+	entropy=$(bc -l <<< "($entropy * 100)")
+    	entropyp=$(bc -l <<< "($entropyp * 100)")
+       	echo -e "$line \t $entropyp" >> $2
     
-    echo -e "$line \t $entropyp" >> $2
-    
-    nbKey=$(($nbKey+1))
-    echo $nbKey
+    	nbKey=$(($nbKey+1))
+    	echo $nbKey
     
 done < $1
-
 exit 0

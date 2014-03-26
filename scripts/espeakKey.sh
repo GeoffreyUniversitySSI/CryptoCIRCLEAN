3c6d806277cb0eec2445dd85281a5024f39940a0
GeoffreyUniversitySSI <guldner6u@live.fr>
2014-03-26 14:26:03 +0100

    Update espeakKey.sh
    correction script

--- scripts/espeakKey.sh
+++ scripts/espeakKey.sh
@@ -6,8 +6,8 @@ keyspace=$(echo -e $key | sed 's/./& /g;s/  / /g')
 espeak -vfr -s 1 "voici la clef qui seras répéter 3 fois"
 espeak -vfr -s 1 "Attention 3 2 1 go"
 espeak -vfr -s 1 -k 2 -g 20 --punct="#$%&'()*+,-./:;<=>?@[]^_{|}~" "$keyspace"
-espeak -ven -s 1"repeat"
+espeak -ven -s 1 "repeat"
 espeak -vfr -s 1 -k 2 -g 20 --punct="#$%&'()*+,-./:;<=>?@[]^_{|}~" "$keyspace"
-espeak -vde -s 1"wiederholen"
+espeak -vde -s 1 "wiederholen"
 espeak -vfr -s 1 -k 2 -g 20 --punct="#$%&'()*+,-./:;<=>?@[]^_{|}~" "$keyspace"
 echo $keyspace
